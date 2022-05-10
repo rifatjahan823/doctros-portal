64a33c33467f25968017792e9972fe2a7097593c
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-10 06:55:02 +0600

    add navebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,21 @@ module.exports = {
   theme: {
     extend: {},
   },
+  
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        doctorsPortal: {
+          primary:"#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "cupcake",
+    ],
+  },
 }
+
